3ce37d5279677e63fce554859377b53fd29ae46f
James Turner <jturner.code@gmail.com>
2021-06-12 07:07:55 -0500

    added close modal js code

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ function hide_el() {
 
 function focus() {
   document.getElementById("searchbox").focus();
-  console.log("i was triggered");
 }
 
 function showmodal() {
@@ -32,35 +31,37 @@ function closemodal() {
   mod.classList.remove("is-active");
 }
 
-function addoption(opt_value) {
-  let msel = document.getElementById("s2");
-  // go by: (<option value="volvo">Volvo</option>)
+// POPULATE A SELECT BOX VIA MODAL
+// function addoption(opt_value) {
+//   let msel = document.getElementById("s2");
+//   // go by: (<option value="volvo">Volvo</option>)
 
-  // CLEAR ANY EXISITNG OPTIONS FIRST
-  while (msel.firstChild) {
-    msel.removeChild(msel.firstChild);
-  }
+//   // CLEAR ANY EXISITNG OPTIONS FIRST
+//   while (msel.firstChild) {
+//     msel.removeChild(msel.firstChild);
+//   }
 
-  //  More secure way to add html versus 'innerhtml'
-  // the below does not work becasue material css uses
-  const opt = document.createElement("option");
-  opt.innerText = opt_value;
-  opt.value = opt_value;
-  opt.selected = true;
-  // opt.className = "red-text";
-  msel.append(opt);
+//   //  More secure way to add html versus 'innerhtml'
+//   // the below does not work becasue material css uses
+//   const opt = document.createElement("option");
+//   opt.innerText = opt_value;
+//   opt.value = opt_value;
+//   opt.selected = true;
+//   // opt.className = "red-text";
+//   msel.append(opt);
 
-  // Re-init FormSelect to show appened item
-  M.FormSelect.init(msel);
+//   // Re-init FormSelect to show appened item
+//   M.FormSelect.init(msel);
 
-  // let dd = M.FormSelect.getInstance(msel);
+//   // let dd = M.FormSelect.getInstance(msel);
 
-  // console.log(dd.getSelectedValues());
+//   // console.log(dd.getSelectedValues());
 
-  let mod = document.getElementById("modal1");
-  M.Modal.getInstance(mod).close();
-}
+//   let mod = document.getElementById("modal1");
+//   M.Modal.getInstance(mod).close();
+// }
 
+// FILTER SEARCH TEXT BOX
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("searchbox");
@@ -77,22 +78,35 @@ function filterFunction() {
   }
 }
 
+// ADD EVENT LISTENERS TO LISTENER CLASS
+// // **window.onload with anonymous func is needed to add
+// // event listensers to html file via external .js file
+// window.onload = function () {
+//   // ADD CLICK EVENT LISTENER TO LISTEN CLASS
+//   document.querySelectorAll(".listen").forEach((item) => {
+//     item.addEventListener("click", (event) => {
+//       // Note two ways to get the innerText
+//       // first via the click event
+//       console.log(event.target.innerText);
+
+//       // second via the item that was clicked on
+//       console.log(item.innerText);
+
+//       addoption(item.innerText);
+//     });
+
+//     // item.addEventListener("click", add);
+//   });
+// };
+
+// ADD EVENT LISTENERS ONLOAD
 // **window.onload with anonymous func is needed to add
 // event listensers to html file via external .js file
 window.onload = function () {
   // ADD CLICK EVENT LISTENER TO LISTEN CLASS
-  document.querySelectorAll(".listen").forEach((item) => {
+  document.querySelectorAll(".closemodal").forEach((item) => {
     item.addEventListener("click", (event) => {
-      // Note two ways to get the innerText
-      // first via the click event
-      console.log(event.target.innerText);
-
-      // second via the item that was clicked on
-      console.log(item.innerText);
-
-      addoption(item.innerText);
+      closemodal();
     });
-
-    // item.addEventListener("click", add);
   });
 };
